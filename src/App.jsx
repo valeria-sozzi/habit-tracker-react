e0bcaae1c393908393d109befff58617027b810d
Valeria Sozzi <valeriasozzi24@gmail.com>
2024-08-19 16:39:25 +0200

    add habit tracker

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,134 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.scss";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [habits, setHabits] = useState([
+    { name: "Fare esercizio fisico", count: 3 },
+    { name: "Leggere un libro", count: 1 },
+    { name: "Meditazione", count: 5 },
+  ]);
+
+  const [inputValue, setInputValue] = useState("");
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+    <div className="app-container">
+      <div className="habit">
+        <label htmlFor="new-habit">Nuova abitudine</label>
+        <input
+          type="text"
+          name="new-habit"
+          id="new-habit"
+          value={inputValue}
+          onChange={(e) => {
+            setInputValue(e.target.value);
+          }}
+        />
+        <button
+          className="btn"
+          onClick={() => {
+            //creo un nuovo oggetto
+            const newHabit = { name: inputValue, count: 0 };
+
+            //creo un nuovo array con l'aggiunta del nuovo oggetto
+            const newHabits = [...habits, newHabit];
+
+            //modifico lo stato dell'array iniziale
+            setHabits(newHabits);
+          }}
+        >
+          Aggiungi nuova abitudine
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+
+      {/* stampiamo la lista a schermo */}
+      <ul>
+        {
+          /* facciamo un ciclo suoi dati per creare il nostro item */
+          habits.map((habit, currentIndex) => (
+            <li key={currentIndex}>
+              {habit.name}
+              <span> conteggio: {habit.count}</span>
+              <button
+                onClick={() => {
+                  //incrementare il contatore dell'abitudine corrente
+
+                  //devo creare un nuovo array
+                  const newHabits = [...habits];
+
+                  //ciclo nell'array per comparare la posizione dell'index
+                  newHabits.map((habit, index) => {
+                    if (currentIndex === index) {
+                      //trovato l'elemento nella posizione corrente incremento il conteggio di uno
+                      habit.count += 1;
+                    }
+                  });
+
+                  //faccio il setHabits con il nuovo array
+                  setHabits(newHabits);
+                }}
+              >
+                +
+              </button>
+              <button
+                onClick={() => {
+                  //decrementare il contatore dell'abitudine corrente
+
+                  //devo creare un nuovo array
+                  const newHabits = [...habits];
+
+                  //ciclo nell'array per comparare la posizione dell'index
+                  newHabits.map((habit, index) => {
+                    if (currentIndex === index) {
+                      //trovato l'elemento nella posizione corrente decrementare il conteggio di uno
+                      habit.count -= 1;
+                    }
+                  });
+
+                  //faccio il setHabits con il nuovo array
+                  setHabits(newHabits);
+                }}
+              >
+                -
+              </button>
+              <button
+                onClick={() => {
+                  //resettare il contatore dell'abitudine corrente
+
+                  //devo creare un nuovo array
+                  const newHabits = [...habits];
+
+                  //ciclo nell'array per comparare la posizione dell'index
+                  newHabits.map((habit, index) => {
+                    if (currentIndex === index) {
+                      //trovato l'elemento nella posizione corrente resetto il conteggio
+                      habit.count = 0;
+                    }
+                  });
+
+                  //faccio il setHabits con il nuovo array
+                  setHabits(newHabits);
+                }}
+              >
+                reset
+              </button>
+              <span
+                onClick={() => {
+                  //devo creare un nuovo array
+                  const newHabits = [...habits];
+                  //uso splice nell'array nuovo per rimuovere il solo elemento corrente
+                  newHabits.splice(currentIndex, 1);
+                  //faccio il setHabits con il nuovo array
+                  setHabits(newHabits);
+                }}
+              >
+                X
+              </span>
+            </li>
+          ))
+        }
+      </ul>
+    </div>
+  );
 }
 
-export default App
+export default App;
